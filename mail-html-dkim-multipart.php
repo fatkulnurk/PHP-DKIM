142ea6e57849075d9b3ae588f8acec16d4283c5e
breakermind <hello@breakermind.com>
2017-09-26 15:13:05 +0100

    How to send PHP multipart mime message with DKIM
    How to send PHP multipart mime message with DKIM

--- mail-html-dkim-multipart.php
+++ mail-html-dkim-multipart.php
@@ -1,5 +1,5 @@
 <?php
-// Send simple html message
+// Send multipart mime message with dkim
 
 // YOUR E-MAIL
 $name = 'Qflash.pl';
@@ -25,7 +25,6 @@ $content = file_get_contents($file);
 $content = chunk_split(base64_encode($content));
 $uid = md5(uniqid(time()));
 $filename = basename($file);
-
 
 $boundary = microtime();
 $header = "From: ".$name." <".$sender.">\r\n";
@@ -63,48 +62,9 @@ $signature = new mail_signature(
 // text/html
 $signed_headers = $signature -> get_signed_headers($to, $subject, $message, $headers);
 
-
 // send message
 ini_set('sendmail_from', $sender);
 echo mail($to, $subject, $message, $signed_headers.$headers);
 echo ' Send with attachment '.$filename;
 // script end
 die();
-
-
-// 3) OR USE OPTIONS TO ADD SOME FLAVOR :
-
-$message = preg_replace('/(?<!\r)\n/', "\r\n", $message);
-$headers = preg_replace('/(?<!\r)\n/', "\r\n", $headers);
-
-$options = array(
-	'use_dkim' => false,
-	'use_domainKeys' => true,
-	'identity' => MAIL_IDENTITY,
-	// if you prefer simple canonicalization (though the default "relaxed"
-	// is recommended)
-	'dkim_body_canonicalization' => 'simple',
-	'dk_canonicalization' => 'nofws',
-	// if you want to sign the mail on a different list of headers than the
-	// default one (see class constructor). Case-insensitive.
-	'signed_headers' => array(
-		'message-Id',
-		'Content-type',
-		'To',
-		'subject'
-	)
-);
-
-require_once 'mail-signature.class.php';
-require_once 'mail-signature.config.php';
-
-$signature = new mail_signature(
-	MAIL_RSA_PRIV,
-	MAIL_RSA_PASSPHRASE,
-	MAIL_DOMAIN,
-	MAIL_SELECTOR,
-	$options
-);
-$signed_headers = $signature -> get_signed_headers($to, $subject, $message, $headers);
-
-// echo mail($to, $subject, $message, $signed_headers.$headers);
